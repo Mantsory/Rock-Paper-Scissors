48861edd33cd4549b0c7a4163b52b0f1966fb430
Mantsory <86209588+Mantsory@users.noreply.github.com>
2025-09-30 10:05:09 -0400

    Submitted version 1.1 of Rock Paper Scissors
    -Made some variables constants
    -Changes the name of the game method to getGameResult
    -Made two new methods for getting the player input and the computers choice
    -Changed all if,else-if statements to switches

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -8,64 +8,130 @@
 import java.util.Scanner;
 
 public class RockPaperScissors {
-    public static String game(String args) {
 
-        //Declares some starting variables
-        String winMSG = "You won the game!"; //declares the message if you win
-        String losMSG = "You lost the game! Maybe you have better luck next time."; //declares the message if you lose
-        String tieMSG = "The game was a tie!"; //declares the message if its a tie
-        String again; //for playing again
-        String compChoice; //this will be the computers choice
-        String choice = args; //Redeclares the choice String
+    //Declares constants
+    public static final String WIN_MSG = "You won the game!"; //declares the message if you win
+    public static final String LOS_MSG = "You lost the game! Maybe you have better luck next time."; //declares the message if you lose
+    public static final String TIE_MSG = "The game was a tie!"; //declares the message if it's a tie
+
+    public static void error() {
+        System.out.println("Something went wrong. Exiting program...");
+        System.exit(1);
+    }//end of error
+
+    public static String getPlayerChoice() {
 
         Scanner input = new Scanner(System.in);
 
+        String playerChoice;
+
+        String playerInput = input.next();
+        String playerInputChar = playerInput.substring(0,1);
+        playerInputChar = playerInputChar.toUpperCase();
+        String overflow = input.nextLine();
+        switch (playerInputChar) {
+            case "R":
+                playerChoice = "rock";
+                break;
+            case "P":
+                playerChoice = "paper";
+                break;
+            case "S":
+                playerChoice = "scissors";
+                break;
+            default:
+                playerChoice = null;
+                System.out.println("You input a improper input. Exiting program...");
+                System.exit(1);
+        }
+        return playerChoice;
+    } //end of getPlayerChoice
+
+    public static String getCompChoice() {
+
+        String compChoice; //this will be the computers choice
+
         //Determines the computers choice
-        int compChoiceInt = (int) (Math.random() * 3) + 1;
-        if (compChoiceInt == 1) {
-            compChoice = "rock";
-        } else if (compChoiceInt == 2) {
-            compChoice = "paper";
-        } else if (compChoiceInt == 3) {
-            compChoice = "scissors";
-        } else {
-            System.out.println("Exit");
-            compChoice = null;
-            System.exit(1);
+        int randInt = (int) (Math.random() * 3) + 1;
+        switch (randInt) {
+            case 1:
+                compChoice = "rock";
+                break;
+            case 2:
+                compChoice = "paper";
+                break;
+            case 3:
+                compChoice = "scissors";
+                break;
+            default:
+                compChoice = null;
+                error();
+
         }
+        return compChoice;
+    }//end of getCompChoice
 
-        choice = choice.toLowerCase();
+    public static String getGameResult(String choice, String compChoice) {
 
-        System.out.println("You selected: " + choice);
-        System.out.println("The computer selected: " + compChoice);
+        Scanner input = new Scanner(System.in);
 
-        if (choice.equalsIgnoreCase(compChoice)) {
-            return tieMSG;
+        String gameResult;
 
-        } else if (choice.matches("rock")) {
-            if (compChoice.matches("paper")) {
-                return losMSG;
-            } else {
-                return winMSG;
-            }
-        } else if (choice.matches("paper")) {
-            if (compChoice.matches("scissors")) {
-                return losMSG;
-            } else {
-                return winMSG;
-            }
-        } else if (choice.matches("scissors")) {
-            if (compChoice.matches("rock")) {
-                return losMSG;
-            } else {
-                return winMSG;
-            }
-        } else {
-            System.out.println("Couldn't play the game with your choice.");
-            return "Game failed.";
+        switch (choice) {
+            case "rock":
+                switch (compChoice) {
+                    case "rock":
+                        gameResult = TIE_MSG;
+                        break;
+                    case "paper":
+                        gameResult = LOS_MSG;
+                        break;
+                    case "scissors":
+                        gameResult = WIN_MSG;
+                        break;
+                    default:
+                        gameResult = null;
+                        error();
+                }
+            break;
+            case "paper":
+                switch (compChoice) {
+                    case "rock":
+                        gameResult = WIN_MSG;
+                        break;
+                    case "paper":
+                        gameResult = TIE_MSG;
+                        break;
+                    case "scissors":
+                        gameResult = LOS_MSG;
+                        break;
+                    default:
+                        gameResult = null;
+                        error();
+                }
+            break;
+            case "scissors":
+                switch (compChoice) {
+                    case "rock":
+                        gameResult = LOS_MSG;
+                        break;
+                    case "paper":
+                        gameResult = WIN_MSG;
+                        break;
+                    case "scissors":
+                        gameResult = TIE_MSG;
+                        break;
+                    default:
+                        gameResult = null;
+                        error();
+                }
+            break;
+            default:
+                gameResult = null;
+                error();
         }
-
-    } //end of game
+        return gameResult;
+    } //end of getGameResult
 
 
     public static void main(String[] args) {
@@ -75,22 +141,26 @@ public class RockPaperScissors {
         boolean gameActive = true;
         while (gameActive){
             System.out.println("Welcome to Rock Paper Scissors!");
-            System.out.println("Please type in 'rock', 'paper', or 'scissors' to play.\n");
+            System.out.println("Please type in 'R', 'P', or 'S' to play.\n");
 
-            String choice = input.next();
-            String overflow = input.nextLine(); //removes any extra text
+            String choice = getPlayerChoice();
+            String compChoice = getCompChoice();
+            String gameResult = getGameResult(choice, compChoice);
 
-            System.out.println(game(choice));
+            System.out.println("You selected: " + choice);
+            System.out.println("The computer selected: " + compChoice);
+            System.out.println(gameResult);
             System.out.println("Would you like to play again?");
             System.out.println("[YES/NO]");
 
             String again = input.next();
-            overflow = input.nextLine(); //removes any extra text
+            String overflow = input.nextLine(); //removes any extra text
 
             if (again.equalsIgnoreCase("yes")) {
                 System.out.println("PLaying again!");
             }
             else {
+                System.out.println("You ended the game. Exiting program...");
                 gameActive = false;
             }
         } //end of while loop
